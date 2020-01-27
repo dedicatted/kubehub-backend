a2dc9206db93ee0db7c613a5ece245d6cdd94a8c
KostiaLevchenko <sigmund313@gmail.com>
2020-01-27 18:00:37 +0200

    grant access for frontend

--- kubehub_backend/settings.py
+++ kubehub_backend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL=True
 
 # Application definition
 
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'kubehub_backend.urls'
