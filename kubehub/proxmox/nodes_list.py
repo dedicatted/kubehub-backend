ad6a91576d0aecba050424fd4fd7cd751003467d
KostiaLevchenko <sigmund313@gmail.com>
2020-01-30 17:00:38 +0200

    branch code style refactor

--- kubehub/proxmox/nodes_list.py
+++ kubehub/proxmox/nodes_list.py
@@ -5,10 +5,10 @@ import json
 
 
 @csrf_exempt
-def node_list(requst):
-    if requst.method == 'POST':
+def nodes_list(request):
+    if request.method == 'POST':
         try:
-            data = json.loads(requst.body)
+            data = json.loads(request.body)
             proxmox = ProxmoxAPI(host=data["proxmox_ip"], user='root@pam', password=data["password"], verify_ssl=False)
             nodes = proxmox.nodes.get()
         except Exception as e:
--- kubehub/urls.py
+++ kubehub/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from .views import crud_views
 from .views import kubespray_deploy
-from .proxmox import NodeList
-from .proxmox import VmList
-from .proxmox import VmDelete
+from .proxmox import nodes_list
+from .proxmox import vm_list
+from .proxmox import vm_delete
 from .proxmox import vm_clone
 
 
@@ -13,10 +13,10 @@ urlpatterns = [
     url(r'^remove$', crud_views.cloud_provider_remove, name='cloud_provider_remove'),
     url(r'^edit$', crud_views.cloud_provider_edit, name='cloud_provider_edit'),
     url(r'^cluster/create/$', kubespray_deploy.kubespray_deploy, name='cluster_create'),
-    url(r'^nodes/list$', NodeList.node_list, name='proxmox_nodes_list'),
-    url(r'^vm/list$', VmList.vm_list, name='proxmox_vm_list'),
-    url(r'^vm/delete$', VmDelete.vm_delete, name='proxmox_vm_delete'),
-    url(r'^vm/clone$', vm_clone.vm_clone, name='proxmox_vm_clone')
+    url(r'^proxmox/nodes/list$', nodes_list.nodes_list, name='proxmox_nodes_list'),
+    url(r'^proxmox/vm/list$', vm_list.vm_list, name='proxmox_vm_list'),
+    url(r'^proxmox/vm/delete$', vm_delete.vm_delete, name='proxmox_vm_delete'),
+    url(r'^proxmox/vm/clone$', vm_clone.vm_clone, name='proxmox_vm_clone')
 ]
 
 
