8df4dff5de96b79616277dcaf8ec9b2f5f0f0185
KostiaLevchenko <sigmund313@gmail.com>
2020-01-30 17:02:37 +0200

    branch code style refactor

--- kubehub/urls.py
+++ kubehub/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     url(r'^remove$', crud_views.cloud_provider_remove, name='cloud_provider_remove'),
     url(r'^edit$', crud_views.cloud_provider_edit, name='cloud_provider_edit'),
     url(r'^cluster/create/$', kubespray_deploy.kubespray_deploy, name='cluster_create'),
-    url(r'^proxmox/nodes/list$', nodes_list.nodes_list, name='proxmox_nodes_list'),
-    url(r'^proxmox/vm/list$', vm_list.vm_list, name='proxmox_vm_list'),
-    url(r'^proxmox/vm/delete$', vm_delete.vm_delete, name='proxmox_vm_delete'),
-    url(r'^proxmox/vm/clone$', vm_clone.vm_clone, name='proxmox_vm_clone')
+    url(r'^nodes/list$', nodes_list.nodes_list, name='proxmox_nodes_list'),
+    url(r'^vm/list$', vm_list.vm_list, name='proxmox_vm_list'),
+    url(r'^vm/delete$', vm_delete.vm_delete, name='proxmox_vm_delete'),
+    url(r'^vm/clone$', vm_clone.vm_clone, name='proxmox_vm_clone')
 ]
 
 
