415f50a1c1a4eaa8ece57a1e9a359fee29aa094b
KostiaLevchenko <sigmund313@gmail.com>
2020-01-31 13:42:26 +0200

    code style refactor

--- kubehub/models/cloud_provider.py
+++ kubehub/models/cloud_provider.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CloudProvider(models.Model):
     CP_TYPES = (
-        ('PROXMOX', 'PROXMOX'),
+        ('Proxmox', 'Proxmox'),
         ('AWS', 'AWS'),
         ('GCP', 'GCP')
     )
