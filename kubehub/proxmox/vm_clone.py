2e20c75c4ea5b1ae8fd1c58a5d9b9431e7f69f49
KostiaLevchenko <sigmund313@gmail.com>
2020-02-05 16:34:40 +0200

    proxmox functions refactoring

--- kubehub/proxmox/vm_clone.py
+++ kubehub/proxmox/vm_clone.py
@@ -1,18 +1,8 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from proxmoxer import ProxmoxAPI
-import json
 
 
-@csrf_exempt
-def vm_clone(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            proxmox = ProxmoxAPI(host=data["proxmox_ip"], user='root@pam', password=data["password"], verify_ssl=False)
-            template = proxmox.nodes(data["node"]).qemu(data["vmid"])
-            clone = template.clone.create(newid=data["newid"], full=data["full"], name=data["name"])
-        except Exception as e:
-            return JsonResponse(e.args, safe=False)
-        else:
-            return JsonResponse(clone, safe=False)
+def vm_clone(proxmox_ip, password, node, vmid, newid, name):
+    proxmox = ProxmoxAPI(host=proxmox_ip, user='root@pam', password=password, verify_ssl=False)
+    template = proxmox.nodes(node).qemu(vmid)
+    clone = template.clone.create(newid=newid, full='1', name=name)
+    return clone
--- kubehub/urls.py
+++ kubehub/urls.py
@@ -5,9 +5,7 @@ from .views import vm_group_crud_view
 from .proxmox import node_list
 from .proxmox import vm_list
 from .proxmox import vm_delete
-from .proxmox import vm_clone
 from .proxmox import get_vm_ip
-from .proxmox import vm_start
 from .proxmox import create_cluster_node
 from .proxmox import vm_manager_status
 from .proxmox import create_vm_cluster
@@ -19,19 +17,20 @@ urlpatterns = [
     url(r'^remove$', crud_views.cloud_provider_remove, name='cloud_provider_remove'),
     url(r'^edit$', crud_views.cloud_provider_edit, name='cloud_provider_edit'),
     url(r'^cluster/create/$', kubespray_deploy.kubespray_deploy, name='cluster_create'),
+
     url(r'^nodes/list$', node_list.node_list, name='proxmox_nodes_list'),
     url(r'^vm/list$', vm_list.vm_list, name='proxmox_vm_list'),
     url(r'^vm/delete$', vm_delete.vm_delete, name='proxmox_vm_delete'),
-    url(r'^vm/clone$', vm_clone.vm_clone, name='proxmox_vm_clone'),
     url(r'^vm/get_ip$', get_vm_ip.vm_ip, name='proxmox_get_vm_ip'),
-    url(r'^vm/start$', vm_start.vm_start, name='proxmox_vm_start'),
+
     url(r'^vm/group/list$', vm_group_crud_view.vm_group_list, name='virtual_machines_group_list'),
     url(r'^vm/group/add$', vm_group_crud_view.vm_group_add, name='virtual_machines_group_add'),
     url(r'^vm/group/remove$', vm_group_crud_view.vm_group_remove, name='virtual_machines_group_remove'),
     url(r'^vm/group/edit$', vm_group_crud_view.vm_group_edit, name='virtual_machines_group_edit'),
     url(r'^vm/get/net/manager/status$', vm_manager_status.vm_manager_status , name='proxmox_get_vm_net_manager_status'),
+
     url(r'^cluster/create/node$', create_cluster_node.create_cluster_node, name='cluster_create_node'),
-    url(r'^cluster/create$', create_vm_cluster.create_vm_cluster, name='get_number_of_cores')
+    url(r'^vm/cluster/create$', create_vm_cluster.create_vm_cluster, name='vm_cluster_create')
 ]
 
 
