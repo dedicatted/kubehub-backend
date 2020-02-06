b83d1cafa17e431ba7f0219bf984067365ad96cb
KostiaLevchenko <sigmund313@gmail.com>
2020-02-06 14:57:07 +0200

    code refactoring

--- kubehub/proxmox/vm_list.py
+++ kubehub/proxmox/vm_list.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from proxmoxer import ProxmoxAPI
+from ..models.cloud_provider import CloudProvider
 import json
 
 
@@ -9,7 +10,8 @@ def vm_list(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            proxmox = ProxmoxAPI(host=data["proxmox_ip"], user='root@pam', password=data["password"], verify_ssl=False)
+            instance = CloudProvider.objects.get(pk=data['cloud_provider_id'])
+            proxmox = ProxmoxAPI(host=instance.api_endpoint, user='root@pam', password=instance.password, verify_ssl=False)
             vm = proxmox.cluster.resources.get(type='vm')
         except Exception as e:
             return JsonResponse(e.args, safe=False)
