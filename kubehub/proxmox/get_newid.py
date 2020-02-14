3315c0fe5568c9b9ffce649a90887078f1ab786f
KostiaLevchenko <sigmund313@gmail.com>
2020-02-14 17:16:10 +0200

    changed range of generated ids

--- kubehub/proxmox/get_newid.py
+++ kubehub/proxmox/get_newid.py
@@ -6,7 +6,7 @@ def get_newid(host, password):
     proxmox = ProxmoxAPI(host=host, user='root@pam', password=password, verify_ssl=False)
     vm_list = proxmox.cluster.resources.get(type='vm')
     vmid_list = [vm["vmid"] for vm in vm_list]
-    newid = random.randint(140, 147)
+    newid = random.randint(100, 500)
     while newid in vmid_list:
-        newid = random.randint(140, 147)
+        newid = random.randint(100, 500)
     return newid
