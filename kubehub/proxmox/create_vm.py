bec22403c922e28ddefdc8bdcbcfcb1da1f9e323
klevchenko <38415607+KostiaLevchenko@users.noreply.github.com>
2020-02-17 10:27:14 +0200

    newid_generator (#14)
    * added functionality to generate unique newid

--- kubehub/proxmox/create_vm.py
+++ kubehub/proxmox/create_vm.py
@@ -9,15 +9,16 @@ from ..proxmox.vm_status import vm_status
 from ..proxmox.get_vm_ip import get_vm_ip
 from ..proxmox.get_less_busy_node import get_less_busy_node
 from ..proxmox.get_vm_node import get_vm_node
+from ..proxmox.get_newid import get_newid
 
 
 def create_vm(data):
     cloud_provider_instance = CloudProvider.objects.get(pk=data['cloud_provider_id'])
     template_instance = Template.objects.get(pk=data['template_id'])
-    newid = random.randint(100, 200)
-    vm_clone(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=template_instance.node, vmid=template_instance.vmid, newid=newid, name=data["name"], target=get_less_busy_node(host=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password))
-    vm_start(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=get_vm_node(host=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, vmid=newid), vmid=newid)
-    status = vm_status(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=get_vm_node(host=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, vmid=newid), vmid=newid)
+    newid = get_newid(host=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password)
+    vm_clone(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=data["node"], vmid=template_instance.vmid, newid=newid, name=data["name"], target='pve-01')
+    vm_start(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=data["node"], vmid=newid)
+    status = vm_status(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=data["node"], vmid=newid)
     while status != "running":
         time.sleep(35)
         vm_start(proxmox_ip=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, node=get_vm_node(host=cloud_provider_instance.api_endpoint, password=cloud_provider_instance.password, vmid=newid), vmid=newid)
