1ff1f27dab99e7a05d844657f58b0a1022edfe63
KostiaLevchenko <sigmund313@gmail.com>
2020-02-18 10:13:06 +0200

    code refactoring

--- kubehub/proxmox/get_newid.py
+++ kubehub/proxmox/get_newid.py
@@ -1,10 +1,13 @@
 from proxmoxer import ProxmoxAPI
+import random
 
 
 def get_newid(host, password):
     proxmox = ProxmoxAPI(host=host, user='root@pam', password=password, verify_ssl=False)
     vm_list = proxmox.cluster.resources.get(type='vm')
     vmid_list = [vm["vmid"] for vm in vm_list]
-    for newid in range(100, 999999999):
-        if newid not in vmid_list:
-            return newid
+    newid = random.randint(100, 500)
+    while newid in vmid_list:
+        newid = random.randint(100, 500)
+    return newid
+
