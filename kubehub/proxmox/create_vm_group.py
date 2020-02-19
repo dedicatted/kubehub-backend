7cca409c41da2fdbe1806902d51d305411417206
klevchenko <38415607+KostiaLevchenko@users.noreply.github.com>
2020-02-19 11:51:44 +0200

    Functional refactoring (#15)
    * implemented ability to create vm group on different nodes
    * changed template list method from sorting by name to sort by key value
    * code refactoring to generate vmid`s in order from 100 to max allowed

--- kubehub/proxmox/create_vm_group.py
+++ kubehub/proxmox/create_vm_group.py
@@ -3,10 +3,11 @@ from multiprocessing.pool import ThreadPool
 import multiprocessing
 
 from ..proxmox.create_vm import create_vm
+from ..proxmox.vmg_data_formation import vmg_data_formation
 
 
 def create_vm_group(data):
     number_of_cores = multiprocessing.cpu_count()
     pool = ThreadPool(number_of_cores)
-    data = [data] * int(data["number_of_nodes"])
-    return pool.map(create_vm, data)
+    return pool.map(create_vm, vmg_data_formation(data))
+
--- kubehub/proxmox/get_template_list.py
+++ kubehub/proxmox/get_template_list.py
@@ -6,5 +6,5 @@ def get_template_list(data):
     instance = CloudProvider.objects.get(pk=data['cloud_provider_id'])
     proxmox = ProxmoxAPI(host=instance.api_endpoint, user='root@pam', password=instance.password, verify_ssl=False)
     vm = proxmox.cluster.resources.get(type='vm')
-    template = list(filter(lambda x: "template" in x["name"], vm))
-    return template
+    template_list = [template for template in vm if template["template"] == 1]
+    return template_list
