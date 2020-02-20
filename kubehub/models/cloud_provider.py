65f7bed210f54b737b8e30568add56889edab612
KostiaLevchenko <sigmund313@gmail.com>
2020-02-20 14:47:17 +0200

    changed maxlength 'cp_type', 'api_endpoint', 'password' added to readonly_fields

--- kubehub/models/cloud_provider.py
+++ kubehub/models/cloud_provider.py
@@ -1,16 +1,21 @@
 from django.db import models
+import subprocess
 
 
 class CloudProvider(models.Model):
+    name_max = int(subprocess.check_output("getconf NAME_MAX /", shell=True))
     CP_TYPES = (
         ('Proxmox', 'Proxmox'),
         ('AWS', 'AWS'),
         ('GCP', 'GCP')
     )
-    cp_type = models.CharField(max_length=20, choices=CP_TYPES)
-    name = models.CharField(max_length=50)
-    api_endpoint = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
+    cp_type = models.CharField(max_length=name_max, choices=CP_TYPES)
+    name = models.CharField(max_length=name_max)
+    api_endpoint = models.CharField(max_length=name_max)
+    password = models.CharField(max_length=name_max)
+    readonly_fields = ('cp_type', 'api_endpoint', 'password')
+
 
     def __str__(self):
-        return f'cloud_provider_id: {self.id}, name: {self.name}, api_endpoint: {self.api_endpoint}, password: {self.password}'
+        return f'cloud_provider_id: {self.id}, name: {self.name}, api_endpoint: {self.api_endpoint}, ' \
+               f'password: {self.password}'
