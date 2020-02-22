c064de776f96c7c896f1325c58ae3a86ad30661e
KostiaLevchenko <sigmund313@gmail.com>
2020-02-22 08:29:19 +0200

    implemented functionality to delete vm group from DB

--- kubehub/urls.py
+++ kubehub/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     url(r'^cluster/create/$', kubespray_deploy.kubespray_deploy, name='cluster_create'),
     url(r'^vm/group/list$', vm_group_view.vm_group_list, name='virtual_machines_group_list'),
     url(r'^vm/group/add$', vm_group_view.vm_group_add, name='virtual_machines_group_add'),
-    url(r'^vm/group/remove$', vm_group_view.vm_group_remove, name='virtual_machines_group_add'),
-
+    url(r'^vm/group/remove$', vm_group_view.vm_group_remove, name='virtual_machines_group_remove'),
     url(r'^template/list$', tamplate_view.list_template, name='proxmox_template_list'),
     url(r'^template/populate$', tamplate_view.populate_template_list, name='proxmox_template_populate'),
 
