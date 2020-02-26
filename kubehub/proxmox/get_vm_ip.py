b09e551a6b5f995eff9ed2efbb1b887c68c6335e
klevchenko <38415607+KostiaLevchenko@users.noreply.github.com>
2020-02-26 11:45:28 +0100

    Speeding up proxmox methods (#20)
    - vmg delete fix and change to decrease removal time
    - 'get ip' method change to decrease vm group creation

--- kubehub/proxmox/get_vm_ip.py
+++ kubehub/proxmox/get_vm_ip.py
@@ -3,6 +3,12 @@ from proxmoxer import ProxmoxAPI
 
 def get_vm_ip(proxmox_ip, password, node, vmid):
     proxmox = ProxmoxAPI(host=proxmox_ip, user='root@pam', password=password, verify_ssl=False)
-    agent = proxmox.nodes(node).qemu(vmid).agent('network-get-interfaces').get()
-    ip = agent.get("result")[1].get("ip-addresses")[0].get("ip-address")
-    return ip
+    status = False
+    while not status:
+        try:
+            agent = proxmox.nodes(node).qemu(vmid).agent('network-get-interfaces').get()
+            ip = agent.get("result")[1].get("ip-addresses")[0].get("ip-address")
+            status = True
+            return ip
+        except Exception:
+            pass
