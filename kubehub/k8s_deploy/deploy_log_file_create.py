77a44ccc2a917f6ec43d367cc6505b3d3810cfab
KostiaLevchenko <sigmund313@gmail.com>
2020-03-09 20:10:28 +0200

    used vm_group id and kubespray deploy id in a file name instead of data

--- kubehub/k8s_deploy/deploy_log_file_create.py
+++ kubehub/k8s_deploy/deploy_log_file_create.py
@@ -1,11 +1,13 @@
-from os import mknod
-from time import gmtime, strftime
+from os import mknod, unlink
+from os.path import exists
 
 
-def create_log_file(log_dir_path):
-    filename = "k8s_deploy_log_#.log"
-    filename = filename.replace("#", strftime("%Y-%m-%d_%H:%M:%S", gmtime()))
+def create_log_file(log_dir_path, k8s_cluster_id, vm_group_id):
+    filename = "kubespray_deploy_#.log"
+    filename = filename.replace("#", f"k8s_cluster_id_{k8s_cluster_id}_vm_group_id_{vm_group_id}")
     fullpath = log_dir_path + filename
+    if exists(fullpath):
+        unlink(fullpath)
     mknod(fullpath)
     return fullpath
 
