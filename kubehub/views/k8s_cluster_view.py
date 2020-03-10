2b85a24fc1210cd0da7aca79bf56835ce59c32ba
KostiaLevchenko <sigmund313@gmail.com>
2020-03-10 13:31:04 +0200

    implemented ability to remove k8s cluster from DB

--- kubehub/views/k8s_cluster_view.py
+++ kubehub/views/k8s_cluster_view.py
@@ -10,27 +10,47 @@ from ..serializers.k8s_cluster_serializer import KubernetesClusterSerializer
 
 @csrf_exempt
 def kubernetes_cluster_list(request):
-    k8s_clusters = []
-    for k8s_cluster in KubernetesCluster.objects.all():
-        kubespray_deploy_list = KubesprayDeploy.objects.filter(k8s_cluster=k8s_cluster)
-        k8s_cluster_dict = model_to_dict(k8s_cluster)
-        k8s_cluster_dict["kubespray_deployments"] = [
-            model_to_dict(kubespray_deploy)
-            for kubespray_deploy in kubespray_deploy_list
-        ]
-        k8s_clusters.append(k8s_cluster_dict)
-    return JsonResponse({'kubernetes_cluster_list': k8s_clusters})
+    if request.method == 'GET':
+        try:
+            k8s_clusters = []
+            for k8s_cluster in KubernetesCluster.objects.all():
+                kubespray_deploy_list = KubesprayDeploy.objects.filter(k8s_cluster=k8s_cluster)
+                k8s_cluster_dict = model_to_dict(k8s_cluster)
+                k8s_cluster_dict["kubespray_deployments"] = [
+                    model_to_dict(kubespray_deploy)
+                    for kubespray_deploy in kubespray_deploy_list
+                ]
+                k8s_clusters.append(k8s_cluster_dict)
+            return JsonResponse({'kubernetes_cluster_list': k8s_clusters})
+        except Exception as e:
+            return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
 
 
 @csrf_exempt
 def kubernetes_cluster_add(request):
     if request.method == 'POST':
-        kubernetes_cluster = loads(request.body)
-        kcs = KubernetesClusterSerializer(data=kubernetes_cluster)
-        if kcs.is_valid():
-            kcs.create(kcs.validated_data)
-            response = dict(kcs.validated_data)
-            response['vm_group'] = model_to_dict(response['vm_group'])
-            return JsonResponse(response)
-        else:
-            return JsonResponse({'errors': kcs.errors})
+        try:
+            kubernetes_cluster = loads(request.body)
+            kcs = KubernetesClusterSerializer(data=kubernetes_cluster)
+            if kcs.is_valid():
+                kcs.create(kcs.validated_data)
+                response = dict(kcs.validated_data)
+                response['vm_group'] = model_to_dict(response['vm_group'])
+                return JsonResponse(response)
+            else:
+                return JsonResponse({'errors': kcs.errors})
+        except Exception as e:
+            return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
+
+
+@csrf_exempt
+def kubernetes_cluster_remove(request):
+    if request.method == 'POST':
+        try:
+            data = loads(request.body)
+            pk = data.get('k8s_cluster_id')
+            instance = KubernetesCluster.objects.get(pk=pk)
+            instance.delete()
+            return JsonResponse({'deleted': model_to_dict(instance)})
+        except Exception as e:
+            return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
