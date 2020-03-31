3bbd69eda0182a04825c0770a831501f2ac5597d
KostiaLevchenko <sigmund313@gmail.com>
2020-03-31 18:35:52 +0300

    fixed config file deleting on re-request same file

--- kubehub/k8s_config/k8s_config_file_create.py
+++ kubehub/k8s_config/k8s_config_file_create.py
@@ -1,15 +1,16 @@
-from os import mknod, unlink
+from os import unlink
 from os.path import exists
 
 
 def create_config_file(config_dir_path, k8s_cluster_id, config_data):
-    filename = f"config_{k8s_cluster_id}.log"
+    filename = f'config_{k8s_cluster_id}.log'
     fullpath = config_dir_path + filename
     if exists(fullpath):
         unlink(fullpath)
+        with open(fullpath, 'a') as file:
+            file.write(config_data.get('content'))
     else:
-        file = open(fullpath, "w")
-        file.write(config_data.get('content'))
-        file.close()
+        with open(fullpath, 'a') as file:
+            file.write(config_data.get('content'))
     return fullpath
 
