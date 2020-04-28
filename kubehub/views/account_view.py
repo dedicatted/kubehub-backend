ba0b7d59c841ff7cf1fa33b41f15e8d52d6e4ce7
klevchenko <sigmund313@gmail.com>
2020-04-28 17:27:35 +0300

    implemented endpoint to get account data by user id

--- kubehub/views/account_view.py
+++ kubehub/views/account_view.py
@@ -2,11 +2,15 @@ from json import loads
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import api_view, permission_classes
 
 from kubehub.models.account import Account
 from kubehub.serializers.account_serializer import RegistrationSerializer
 
 
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
 @csrf_exempt
 def account_list(request):
     if request.method == 'GET':
@@ -16,6 +20,8 @@ def account_list(request):
             return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
 
 
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
 @csrf_exempt
 def account_add(request):
     if request.method == 'POST':
@@ -31,6 +37,8 @@ def account_add(request):
             return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
 
 
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
 @csrf_exempt
 def account_remove(request):
     if request.method == 'POST':
@@ -44,6 +52,8 @@ def account_remove(request):
             return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
 
 
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
 @csrf_exempt
 def account_edit(request):
     if request.method == 'POST':
@@ -57,5 +67,19 @@ def account_edit(request):
                 return JsonResponse(model_to_dict(user))
             else:
                 return JsonResponse({'errors': users.errors})
+        except Exception as e:
+            return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+@csrf_exempt
+def get_account_data(request):
+    if request.method == 'POST':
+        try:
+            data = loads(request.body)
+            user_id = data.pop('id')
+            user_data = Account.objects.get(pk=user_id)
+            return JsonResponse(model_to_dict(user_data))
         except Exception as e:
             return JsonResponse({'errors': {f'{type(e).__name__}': [str(e)]}})
