b583bd1bc918153020d87fb90ff8445991c0288f
klevchenko <sigmund313@gmail.com>
2020-04-29 23:56:16 +0300

    account crud runtime errors resolved

--- kubehub/models/account.py
+++ kubehub/models/account.py
@@ -35,7 +35,7 @@ class Account (AbstractBaseUser):
         blank=True,
         null=True
     )
-    is_admin = models.BooleanField(blank=True, default=False)
+    is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
