338c525cf87d40bb5eedba4da78b7ed45de1ab33
klevchenko <sigmund313@gmail.com>
2020-05-14 21:57:00 +0300

    altered vbox_img_dir field and added machine_folder field

--- kubehub/vbox_api/models/vbox_cloud_provider.py
+++ kubehub/vbox_api/models/vbox_cloud_provider.py
@@ -6,12 +6,17 @@ from kubehub.models.cloud_provider import CloudProvider
 
 class VirtualBoxCloudProvider(CloudProvider):
     name_max = int(check_output('getconf NAME_MAX /', shell=True))
-    vbox_img_dir = models.CharField(
+    image_folder = models.CharField(
         max_length=name_max,
         unique=True,
         null=False
     )
+    machine_folder = models.CharField(
+        max_length=name_max,
+        unique=False,
+        null=False
+    )
 
     def __str__(self):
-        return f'id: {self.id}, vbox_img_dir: {self.vbox_img_dir}, '
+        return f'id: {self.id}, image_folder: {self.image_folder}, machine_folder: {self.machine_folder},'
 
